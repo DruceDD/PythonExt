b9beebac8628c37d53b54f557127ba6a84320829
Druce <raa-07@mail.ru>
2023-07-21 20:13:38 +0300

    homework2

---
+++ seminar2/hw1.py
@@ -0,0 +1,27 @@
+# Напишите программу, которая получает целое число и возвращает его шестнадцатеричное строковое представление.
+# Функцию hex используйте для проверки своего результата.
+
+HEX_DIVIDER = 16
+HEX_STRING = "0123456789abcdef"
+
+
+def get_number_from_user() -> int:
+    r = input("Введите число: ")
+    return int(r)
+
+
+def converter(patient: int) -> str:
+    r: str = ""
+    while patient > 0:
+        r = str(HEX_STRING[patient % HEX_DIVIDER]) + r
+        patient //= HEX_DIVIDER
+    return "Результат: " + r
+
+
+pat = get_number_from_user()
+if isinstance(pat, int):
+    print(converter(pat))
+else:
+    print("Неверно. Попробуйте снова")
+
+print("Проверка: ", hex(pat)[2:])
